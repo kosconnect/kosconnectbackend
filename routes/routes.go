bbfee3bc7c02fe17e31100965855824287e9062d
fathyafathazz <fathyafathazzra@gmail.com>
2025-01-07 09:04:28 +0700

    change callback method from GET into POST

--- routes/routes.go
+++ routes/routes.go
@@ -14,7 +14,7 @@ func AuthRoutes(router *gin.Engine) {
 
 		// Tambahkan routes untuk OAuth Google
 		authGroup.GET("/google/login", controllers.HandleGoogleLogin)
-		authGroup.GET("/callback", controllers.HandleGoogleCallback)
+		authGroup.POST("/callback", controllers.HandleGoogleCallback)
 		authGroup.PUT("/assign-role", controllers.AssignRole)
 	}
 }
